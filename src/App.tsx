09fc77b00d4ae02dbfc03f8c8a87cacc358fde45
Jay Mar <jaymarrebanda123@gmail.com>
2024-01-25 22:18:21 +0800

    starter template w/ daisy ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card from "./components/Card";
+import Navbar from "./components/navbar/Navbar";
+import HeroSection from "./components/section/HeroSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Card />
+    </>
+  );
+}
+
+export default App;
